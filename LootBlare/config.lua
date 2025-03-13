f5d7232d9391aaf918277fa49da1ddc5f106c3f8
SeguisDumble <seguisdumble@gmail.com>
2025-03-13 11:31:27 +0100

    create utils and config

---
+++ LootBlare/config.lua
@@ -0,0 +1,30 @@
+config = {}
+
+config.BUTTON_WIDTH = 32
+config.BUTTON_COUNT = 3
+config.BUTTON_PADING = 10
+config.FONT_NAME = 'Fonts\\FRIZQT__.TTF'
+config.FONT_SIZE = 12
+config.FONT_OUTLINE = 'OUTLINE'
+config.RAID_CLASS_COLORS = {
+  ['Warrior'] = 'FFC79C6E',
+  ['Mage']    = 'FF69CCF0',
+  ['Rogue']   = 'FFFFF569',
+  ['Druid']   = 'FFFF7D0A',
+  ['Hunter']  = 'FFABD473',
+  ['Shaman']  = 'FF0070DE',
+  ['Priest']  = 'FFFFFFFF',
+  ['Warlock'] = 'FF9482C9',
+  ['Paladin'] = 'FFF58CBA',
+}
+config.ADDON_TEXT_COLOR= 'FFEDD8BB'
+config.DEFAULT_TEXT_COLOR = 'FFFFFF00'
+config.SR_TEXT_COLOR = 'FFFF0000'
+config.MS_TEXT_COLOR = 'FFFFFF00'
+config.OS_TEXT_COLOR = 'FF00FF00'
+config.TM_TEXT_COLOR = 'FF00FFFF'
+
+config.LB_PREFIX = 'LootBlare'
+config.LB_GET_DATA = 'get data'
+config.LB_SET_ML = 'ML set to '
+config.LB_SET_ROLL_TIME = 'Roll time set to '
---
+++ LootBlare/utils.lua
@@ -0,0 +1,49 @@
+function lb_print(msg)
+  DEFAULT_CHAT_FRAME:AddMessage('|c' .. config.ADDON_TEXT_COLOR .. 'LootBlare: ' .. msg .. '|r')
+end
+
+function colorMsg(message)
+  msg = message.msg
+  class = message.class
+  _,_,_, message_end = string.find(msg, '(%S+)%s+(.+)')
+  classColor = config.RAID_CLASS_COLORS[class]
+  textColor = config.DEFAULT_TEXT_COLOR
+
+  if string.find(msg, '-101') then
+    textColor = config.SR_TEXT_COLOR
+  elseif string.find(msg, '-100') then
+    textColor = config.MS_TEXT_COLOR
+  elseif string.find(msg, '-99') then
+    textColor = config.OS_TEXT_COLOR
+  elseif string.find(msg, '-50') then
+    textColor = config.TM_TEXT_COLOR
+  end
+
+  colored_msg = '|c' .. classColor .. '' .. message.roller .. '|r |c' .. textColor .. message_end .. '|r'
+  return colored_msg
+end
+
+function tsize(t)
+  c = 0
+  for _ in pairs(t) do
+      c = c + 1
+  end
+  if c > 0 then return c else return nil end
+end
+
+function CheckItem(link)
+  discover:SetOwner(UIParent, 'ANCHOR_PRESERVE')
+  discover:SetHyperlink(link)
+
+  if discoverTextLeft1 and discoverTooltipTextLeft1:IsVisible() then
+    local name = discoverTooltipTextLeft1:GetText()
+    discoverTooltip:Hide()
+
+    if name == (RETRIEVING_ITEM_INFO or '') then
+      return false
+    else
+      return true
+    end
+  end
+    return false
+end
